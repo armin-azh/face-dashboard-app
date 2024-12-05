13b5ff53b9a56de6837930d8847ba0cadbc7801a
Armin Azhdehnia <arminmk18@gmail.com>
2024-12-06 01:07:51 +0330

    add new camera

--- src/api/camera.go
+++ src/api/camera.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"face.com/gateway/src/api/serializers"
 	"github.com/gofiber/fiber/v2"
@@ -22,7 +26,41 @@ func (server *Server) createCamera(c *fiber.Ctx) error{
 	}
 
 	// TODO: Send through rtsp2web server
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "New camera has been created", "code": SUCCESS, "data": camera})
+	url:=fmt.Sprintf("%s/stream/%s,/add", server.config.Rtsp2Web, camera.Prime)
+
+	data := fiber.Map{
+		"name": camera.Name,
+		"channels": fiber.Map{
+			"name": "channel1",
+			"url": camera.Url,
+			"on_demand": true,
+			"debug": false,
+			"status": 0,	
+		},
+	}
+
+	raw, err := json.Marshal(data)
+	if err!=nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Message": "Cannot convert data to json", "code": FAILED})
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(raw))
+	if err!=nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Message": "Cannot create a new request", "code": FAILED})
+	} 
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Message": "Cannot create a new request", "code": FAILED})
+	}
+	
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "New camera has been created", "code": SUCCESS, "data": camera})
+	}
+	
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Cannot create new camera", "code": FAILED})
 }
 
 func (server *Server) getCameraList(c *fiber.Ctx) error{
--- src/api/status.go
+++ src/api/status.go
@@ -1,6 +1,6 @@
 package api
 
-
 const (
 	SUCCESS = "success"
+	FAILED  = "failed"
 )
