4ff040039cd870c20a7751620cde63a27fe10339
Armin Azhdehnia <arminmk18@gmail.com>
2024-12-06 00:40:59 +0330

    add rtsp2 on the configs

--- src/common/config.go
+++ src/common/config.go
@@ -3,22 +3,17 @@ package common
 import "github.com/spf13/viper"
 
 type Config struct {
-	Debug        bool   `mapstructure:"DEBUG"`
-	Port         int    `mapstructure:"PORT"`
-	Host         string `mapstructure:"HOST"`
-	DatabaseUrl  string `mapstructure:"DATABASE_URL"`
-	KafkaBootStr string `mapstructure:"KAFKA_BOOTSTRAP_SERVER"`
-	MediaDir     string `mapstructure:"MEDIA_DIR"`
-
-	// Redis
-	RedisAddr string `mapstructure:"REDIS_ADDR"`
-	RedisPassword string `mapstructure:"REDIS_PASSWORD"`
-
-	// CORS
+	Debug            bool   `mapstructure:"DEBUG"`
+	Port             int    `mapstructure:"PORT"`
+	Host             string `mapstructure:"HOST"`
+	DatabaseUrl      string `mapstructure:"DATABASE_URL"`
+	KafkaBootStr     string `mapstructure:"KAFKA_BOOTSTRAP_SERVER"`
+	MediaDir         string `mapstructure:"MEDIA_DIR"`
+	RedisAddr        string `mapstructure:"REDIS_ADDR"`
+	RedisPassword    string `mapstructure:"REDIS_PASSWORD"`
 	CorsAllowOrigins string `mapstructure:"CORS_ALLOW_ORIGINS"`
-
-	// Time Zone
-	TimeZone string `mapstructure:"Time_Zone"`
+	TimeZone         string `mapstructure:"Time_Zone"`
+	Rtsp2Web         string `mapstructure:"RTSP_2_WEB_ADDR"`
 }
 
 func LoadConfig(path string) (*Config, error) {
