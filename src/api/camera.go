86bdbc06f5519c0c427938f6e7470db64186d9e2
Armin Azhdehnia <arminmk18@gmail.com>
2024-12-06 10:33:11 +0330

    add new camera

--- src/api/camera.go
+++ src/api/camera.go
@@ -9,61 +9,75 @@ import (
 
 	"face.com/gateway/src/api/serializers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/utils"
 )
 
+type Channels struct {
+	Name     string `json:"name"`
+	URL      string `json:"url"`
+	OnDemand bool   `json:"on_demand"`
+	Debug    bool   `json:"debug"`
+	Status   int    `json:"status"`
+}
 
-func (server *Server) createCamera(c *fiber.Ctx) error{
+type RTSP struct {
+	Name string `json:"name"`
+	Channels map[string]Channels
+}
+
+func (server *Server) createCamera(c *fiber.Ctx) error {
 	c.Accepts("application/json")
 	serializer := new(serializers.CameraSerializer)
-	if err:=server.ValidatePayload(serializer, c); err != nil{
+	if err := server.ValidatePayload(serializer, c); err != nil {
 		return err
 	}
 
 	camera, err := server.mainStore.CreateCamera(context.Background(), utils.UUID(), serializer.Name, serializer.Type, serializer.URL, serializer.OnDemand)
-	if err!=nil{
+	if err != nil {
 		return handleSQLError(c, err)
 	}
 
 	// TODO: Send through rtsp2web server
-	url:=fmt.Sprintf("%s/stream/%s,/add", server.config.Rtsp2Web, camera.Prime)
+	url := fmt.Sprintf("%s/stream/%s,/add", server.config.Rtsp2Web, camera.Prime)
 
-	data := fiber.Map{
-		"name": camera.Name,
-		"channels": fiber.Map{
-			"name": "channel1",
-			"url": camera.Url,
-			"on_demand": true,
-			"debug": false,
-			"status": 0,	
+	data := RTSP{
+		Name: camera.Name,
+		Channels: map[string]Channels{
+			"0": {
+				Name: "ch1",
+				URL: camera.Url,
+				OnDemand: camera.OnDemand,
+				Debug: false,
+				Status: 0,
+			},
 		},
 	}
 
 	raw, err := json.Marshal(data)
-	if err!=nil{
+	if err != nil {
+		log.Errorf("json marshal %s", err)
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Message": "Cannot convert data to json", "code": FAILED})
 	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(raw))
-	if err!=nil{
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Message": "Cannot create a new request", "code": FAILED})
-	} 
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil{
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"Message": "Cannot create a new request", "code": FAILED})
+	// Make the POST request
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(raw))
+	if err != nil {
+		log.Fatalf("Error making POST request: %v", err)
 	}
-	
+	defer resp.Body.Close()
+
 	defer resp.Body.Close()
 
 	if resp.StatusCode == 200 {
+
 		return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "New camera has been created", "code": SUCCESS, "data": camera})
 	}
-	
+	log.Errorf("cannot create new camera: %s", resp.StatusCode)
 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Cannot create new camera", "code": FAILED})
 }
 
-func (server *Server) getCameraList(c *fiber.Ctx) error{
+func (server *Server) getCameraList(c *fiber.Ctx) error {
 	params := struct {
 		Page     int32
 		PageSize int32
@@ -78,21 +92,21 @@ func (server *Server) getCameraList(c *fiber.Ctx) error{
 		})
 	}
 
-	cameras, err := server.mainStore.ListCameras(context.Background(),params.PageSize, (params.Page - 1)*params.PageSize)
-	if err != nil{
+	cameras, err := server.mainStore.ListCameras(context.Background(), params.PageSize, (params.Page-1)*params.PageSize)
+	if err != nil {
 		return handleSQLError(c, err)
 	}
 
-	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{"page": params.Page, "page_size": params.PageSize, "results": cameras})
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"page": params.Page, "page_size": params.PageSize, "results": cameras})
 }
 
-func (server *Server) getCameraByPrime(c *fiber.Ctx) error{
-	id:=c.Params("id")
+func (server *Server) getCameraByPrime(c *fiber.Ctx) error {
+	id := c.Params("id")
 
-	camera, err:=server.mainStore.GetCameraByPrime(context.Background(), id)
-	if err != nil{
+	camera, err := server.mainStore.GetCameraByPrime(context.Background(), id)
+	if err != nil {
 		return handleSQLError(c, err)
 	}
 
-	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{"message": "Camera has been retreived", "code": SUCCESS, "data": camera})
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Camera has been retreived", "code": SUCCESS, "data": camera})
 }
--- src/api/serializers/camera.go
+++ src/api/serializers/camera.go
@@ -4,5 +4,5 @@ type CameraSerializer struct {
 	Name     string `validate:"required,min=2,max=50" json:"name"`
 	Type     string `validate:"required,min=2,max=50,oneof=recording entry" json:"type"`
 	URL      string `validate:"required,url" json:"url"`
-	OnDemand bool   `validate:"required" json:"on_demand"`
+	OnDemand bool   `json:"on_demand"`
 }
