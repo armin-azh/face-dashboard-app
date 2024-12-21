5888eb9f1b5437518ad87d6992dae75fe1184062
Armin Azhdehnia <arminmk18@gmail.com>
2024-12-21 21:13:11 +0330

    start react project

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import './styles/App.css'
+
+function Home() {
+
+
+  return <div>
+    <h1 className="text-2xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+}
+
+export default Home
