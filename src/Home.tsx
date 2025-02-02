cbc556f3bfbb2dca26c2e80d5dd8f7f5034977e7
Armin Azhdehnia <arminmk18@gmail.com>
2025-02-02 17:43:07 +0330

    change dashboard header color

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
   }, []);
 
   return <main className="flex-grow pt-16 bg-gray-100 p-4">
-    <h1 className="text-2xl font-extrabold text-gray-800">Dashboard Overview</h1>
+    <h1 className="text-2xl font-semibold text-blue-700 mb-6">Dashboard Overview</h1>
     <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-4">
 
       {/* Overview Widget */}
